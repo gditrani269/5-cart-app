2c6e0c895836bc851d4654a282826e3322a4cf75
unknown <gditrani269@gmail.com>
2024-04-02 16:42:07 -0300

    seccion 6 - 99.Component Cart Routes

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,9 +1,7 @@
 
-import { Routes, Route, Navigate  } from "react-router-dom"
-import { CartView } from "./components/CartView"
-import { CatalogView } from "./components/CatalogView"
 import { useItemsCart } from "./hooks/useItemsCart"
 import { NavBar } from "./components/NavBar"
+import { CartRoutes } from "./routes/CartRoutes"
  
 
 //defino una estructura inicial de items de productos para inicializar el usestate cartitems
@@ -23,31 +21,11 @@ export const CartApp = () => {
             <NavBar />
             <div className="container my-4">
                 <h3>Cart App</h3>
-                <Routes>
-                    <Route 
-                        path="catalog" 
-                        element={ <CatalogView handler= { product => handlerAddProductCart (product)}/> }/>
-                    <Route 
-                        path="cart" 
-                        element={(
-                            cartItems?.length <= 0 ?
-                            <div className="alert alert-warning">No hay productos en el carrito de compras !!</div>
-                            :
-                            (
-                                <div className="my-4 w-50">
-                                    <CartView items ={ cartItems } handlerDelete = {handlerDeleteProductCart}/>
-                                </div>
-                            )
-                        )}/>
-                    {/*navega por default hacia el catalogo, cuando se carga la pagina por ejemplo. Eso lo hace con le Navigate */}
-                    <Route path="/" element={ <Navigate to={'/catalog'} /> } />
-                </Routes>
-                
-
-                {/*para solo mostrar el CartView si hay algo que mostrar ponemos la siguiente logica */}
-                {/* Si es distinto de  null y menor o igual a cero */}
-
-
+                <CartRoutes 
+                    cartItems={cartItems} 
+                    handlerAddProductCart={handlerAddProductCart}
+                    handlerDeleteProductCart={handlerDeleteProductCart}
+                />
             </div>
         </>
     )
